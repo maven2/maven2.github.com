b573bd41f5d0b59838ea793b220fe7b572435d73
Alistair A. Israel <aisrael@gmail.com>
2009-10-28 18:52:37 +0800

    Added MIT License to gentoc.rb; Added XML DOCTYPE

--- gentoc.rb
+++ gentoc.rb
@@ -1,4 +1,27 @@
 #!/usr/bin/env ruby
+#
+# Copyright (c) 2009 Alistair A. Israel
+# 
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
 
 ROOT=`pwd`.chomp
 SCRIPT=File.join(ROOT, File.basename($0))
@@ -53,6 +76,8 @@ def write_index_html(base, dirs, files)
     
   File.open(index, 'w') { |o|
     o << <<END
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <title>#{title}</title>
